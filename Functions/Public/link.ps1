871b55a57133a437cc7dcf05b64f0bea426801d5
stephanevg <svangulick@gmail.com>
2018-03-27 15:41:07 +0200

    Added link attributes

--- Functions/Public/link.ps1
+++ Functions/Public/link.ps1
@@ -1,8 +1,11 @@
 Function Get-Link {
     Param(
+        [Parameter(Mandatory=$true)]
         [String]
         $href,
 
+        [Parameter(Mandatory=$true)]
+        [Validateset("alternate","author","dns-prefetch","help","icon","license","next","pingback","preconnect","prefetch","preload","prerender","prev","search","stylesheet")]
         [string]
         $rel
     )
@@ -14,3 +17,4 @@ Function Get-Link {
 
 
 }
+
