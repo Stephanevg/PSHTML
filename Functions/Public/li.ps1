50ecef51e98251133f1c8441c7cb000f00c71996
Kevin Bates <kevin96bates@gmail.com>
2018-04-10 20:30:14 +0200

    added li function

--- Functions/Public/li.ps1
+++ Functions/Public/li.ps1
@@ -1,165 +1,83 @@
 Function li {
-
     <#
     .SYNOPSIS
-    Generates li HTML tag.
+    Create a li tag in an HTML document.
     
     .EXAMPLE
-    
-    ol -reversed -start 1 -type "typo" -Attributes @{Name="Kevin" ; whatever="floats your boat"} -content {
-        li -content "Test entry" -Class "classy" -value 0 -Style "stylish" -Attributes @{Name="Johnny" ; bibop="bopib"}
-        li "Test entry 2"
-    }
 
-    Generates the following code:
-
-    <ol reversed="True" start="1" type="typo" reversed="true" Name="Kevin" whatever="floats your boat">
-        <li Class="classy" value="0" Style="stylish" bibop="bopib" Name="Johnny">
-            Test entry
-        </li>
-        <li>
-            Test entry 2
-        </li>
-    </ol>
-    
+    li 
     .EXAMPLE
-    
-    It is also possible to use regular powershell logic inside a scriptblock. The example below, generates an ol element with multiple li Elements. 
-    Where every li tag contains a name of a service that starts with "s".
+    li "woop1" -Class "class"
 
-    $Services = Get-Service | ?{$_.name.Startswith("s")}
- 
-    ol {
-        foreach($p in $test){
-            li -content "$p" -Class "classy" -value "asdf" -Style "whatever" -Attributes @{name='asdf'}
-        }
-    }
+    .EXAMPLE
+    li "woop2" -Class "class" -Id "Something"
 
-    Generates the following code:
+    .EXAMPLE
+    li "woop3" -Class "class" -Id "something" -Style "color:red;"
 
-    <ol>
-        <li Class="classy" value="asdf" Style="whatever" name="asdf" >
-            @{Name=seclogon}
-        </li>
-        <li Class="classy" value="asdf" Style="whatever" name="asdf" >
-            @{Name=shpamsvc}
-        </li>
-        <li Class="classy" value="asdf" Style="whatever" name="asdf" >
-            @{Name=smphost}
-        </li>
-        <li Class="classy" value="asdf" Style="whatever" name="asdf" >
-            @{Name=spectrum}
-        </li>
-    </ol>
+    .Notes
+    Author: Kevin Bates
+    Version: 0.1.0
+    History:
+        @bateskevin;v0.1.0;40/10/2018;creation
 
     #>
-
-    [CmdletBinding()]
+    [Cmdletbinding()]
     Param(
-        [Parameter(
-            ValueFromPipeline = $true,
-            Mandatory = $true,
-            Position = 0
-        )]
-        $content,
+        [Parameter(Mandatory=$true)]
+        [AllowEmptyString()]
+        [AllowNull()]
+        [String]
+        $Content,
 
-        [Parameter(Position = 1)]
-        [String]$Class,
+        [AllowEmptyString()]
+        [AllowNull()]
+        [String]$Class="",
 
-        [Parameter(Position = 2)]
-        [String]$value,
+        [String]$Id,
 
-        [Parameter(Position = 3)]
+        [AllowEmptyString()]
+        [AllowNull()]
         [String]$Style,
 
-        [Hashtable]$Attributes
+        [String]$value
     )
-    Process{
-
-        
 
         $attr = ""
-        $boundParams = $PSBoundParameters
-        $CommonParameters = @(
-            "Debug",
-            "ErrorAction",
-            "ErrorVariable",
-            "InformationAction",
-            "InformationVariable",
-            "OutVariable",
-            "OutBuffer",
-            "PipelineVariable",
-            "Verbose",
-            "WarningAction",
-            "WarningVariable",
-            "Attributes"
-        )
+        $CommonParameters = ("Attributes", "content") + [System.Management.Automation.PSCmdlet]::CommonParameters + [System.Management.Automation.PSCmdlet]::OptionalCommonParameters
+        $CustomParameters = $PSBoundParameters.Keys | ? { $_ -notin $CommonParameters }
+        
+        if($CustomParameters){
+            
+            foreach ($entry in $CustomParameters){
 
-        foreach ($cp in $CommonParameters){
-
-            $null = $boundParams.Remove($cp)
-        }
-
-        foreach ($entry in $boundParams.Keys){
-            if ($entry -eq 'content'){
-                continue
+                
+                $Attr += "{0}=`"{1}`" " -f $entry,$PSBoundParameters[$entry]
+    
             }
-            $attr += "$($entry)=`"$($boundParams[$entry])`" "
-
+                
         }
 
         if($Attributes){
-
-            Foreach($key in $Attributes.Keys){
-
-                $attr += '{0}="{1}" ' -f $key,$Attributes[$key]
-     
+            foreach($entry in $Attributes.Keys){
+               
+                $attr += "{0}=`"{1}`" " -f $entry,$Attributes[$Entry]
             }
-           
         }
 
-        $counter = 0
-        
-        if($content.GetType().name -eq "Object[]"){
 
-            foreach($item in $content){
+if($attr){
+    $return = @"
+    <li $attr>$Content</li>
+"@
 
-                if($attr){
-                    "<li $attr>" 
-                }else{
-                    "<li>"
-                }
-    
-                if($content){
-                    $content[$counter]
-                }
-                    
-                $counter ++
-    
-                '</li>'
-            }     
+}else{
 
-        }else{
-
-            if($attr){
-                "<li $attr>" 
-            }else{
-                "<li>"
-            }
-
-            if($content){
-                $content
-            }
-                
-            $counter ++
-
-            '</li>'
-
-        }
-
-        
-    }
-    
-    
+    $return =     @"
+    <li>$Content</li>
+"@
 }
 
+return $return
+
+}
