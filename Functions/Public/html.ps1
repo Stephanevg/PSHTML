e4996916efffdad9ad0adc0b1a47476b41d6e505
stephanevg <svangulick@gmail.com>
2018-04-11 00:09:03 +0200

    updated html to 1.0

--- Functions/Public/html.ps1
+++ Functions/Public/html.ps1
@@ -1,19 +1,104 @@
-Function Html {
-    Param(
-        [Parameter(
-            ValueFromPipeline = $true,
-            Mandatory = $true,
-            Position = 0
-        )]
-        [scriptblock]
-        $ChildItem
-    )
+Function html {
+    <#
+    .SYNOPSIS
+    Generates a html HTML tag.
     
+    .DESCRIPTION
+
+    The <html> tag tells the browser that this is an HTML document.
+
+    The <html> tag represents the root of an HTML document.
+
+    The <html> tag is the container for all other HTML elements (except for the <!DOCTYPE> tag).
+    
+    .PARAMETER Class
+    Allows to specify one (or more) class(es) to assign the html element.
+    More then one class can be assigned by seperating them with a white space.
+
+    .PARAMETER Id
+    Allows to specify an id to assign the html element.
+    
+    .PARAMETER Style
+    Allows to specify in line CSS style to assign the html element.
+
+    .PARAMETER Content
+    Allows to add child element(s) inside the current opening and closing HTML tag(s). 
+
+
+    .EXAMPLE
+    
+    
+    .NOTES
+    Current version 1.0
+    History:
+        2018.04.10;Stephanevg; Added parameters
+        2018.04.01;Stephanevg;Creation.
+
+#>
+Param(
+
+    [Parameter(
+        ValueFromPipeline = $true,
+        Mandatory = $false,
+        Position = 0
+    )]
+    [AllowEmptyString()]
+    [AllowNull()]
+    $Content,
+
+    [Parameter(Position = 1)]
+    [String]$Class,
+
+    [Parameter(Position = 2)]
+    [String]$Id,
+
+    [Parameter(Position = 3)]
+    [String]$Style,
+
+    [Parameter(Position = 4)]
+    [Hashtable]$Attributes 
+)
+$attr = ""
+    $CommonParameters = ("Attributes", "Content") + [System.Management.Automation.PSCmdlet]::CommonParameters + [System.Management.Automation.PSCmdlet]::OptionalCommonParameters
+    $CustomParameters = $PSBoundParameters.Keys | ? { $_ -notin $CommonParameters }
+    
+    if($CustomParameters){
         
-    "<html>"
-       
-        $ChildItem.Invoke()
+        foreach ($entry in $CustomParameters){
+
+            
+            $Attr += "{0}=`"{1}`" " -f $entry,$PSBoundParameters[$entry]
+
+        }
+            
+    }
+
+    if($Attributes){
+        foreach($entry in $Attributes.Keys){
+           
+            $attr += "{0}=`"{1}`" " -f $entry,$Attributes[$Entry]
+        }
+    }
+
+    if($attr){
+        "<html {0} >"  -f $attr
+    }else{
+        "<html>"
+    }
     
-    "</html>"
+  
+
+    if($Content){
+
+        if($Content -is [System.Management.Automation.ScriptBlock]){
+            $Content.Invoke()
+        }else{
+            $Content
+        }
+    }
+        
+
+    '</html>'
+
 
 }
