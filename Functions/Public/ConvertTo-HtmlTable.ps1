3e453d703500cddc60516326c6abddeeddd901c8
stephanevg <svangulick@gmail.com>
2018-05-09 11:53:43 +0200

    v0.4.11: Addedd ConvertTo-HTMLTable.ps1

---
+++ Functions/Public/ConvertTo-HtmlTable.ps1
@@ -0,0 +1,65 @@
+Function ConvertTo-HTMLTable {
+    <#
+
+        .SYNOPSIS
+        Converts a powershell object to a HTML table.
+
+        .DESCRIPTION
+        This cmdlet is intended to be used when powershell objects should be rendered in an HTML table format.
+
+        .EXAMPLE
+        $service = Get-Service -Name Sens,wsearch,wscsvc | select DisplayName,Status,StartType
+        ConvertTo-HTMLtable -Object $service 
+
+        .EXAMPLE
+
+        $proc = Get-Process | select -First 2
+        ConvertTo-HTMLtable -Object $proc
+
+        .NOTES
+        Current version 0.6
+        History:
+           2018.05.09;stephanevg;Creation.
+    #>
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true,
+                ValueFromPipeline=$true
+        )]
+        $Object
+    )
+
+    
+    Table{
+        
+        $Properties = $object | get-member | where {$_.MemberType -eq 'property' -or $_.MemberType -eq 'NoteProperty'}
+
+        thead {
+            tr{
+
+
+                foreach($prop in $Properties.Name){
+            
+                    td{ 
+                        $prop
+                    }
+                }
+            }
+        }
+        Tbody{
+            foreach($item in $object){
+                tr{
+    
+                    
+                    foreach($propertyName in $Properties.Name){
+        
+                        td {
+                            $item.$propertyName
+                        }
+                
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Tests/ConvertTo-HtmlTable.Tests.ps1
@@ -0,0 +1,74 @@
+$TestsPath = Split-Path $MyInvocation.MyCommand.Path
+
+$RootFolder = (get-item $TestsPath).Parent
+
+Push-Location -Path $RootFolder.FullName
+
+set-location -Path $RootFolder.FullName
+
+Write-Verbose "Importing module"
+
+import-module .\PSHTML -Force
+
+
+Describe "Testing ConvertTo-HTMLTable" {
+    
+
+    $Class = "MyClass"
+    $Id = "MyID"
+    $Style = "Background:green"
+    $CustomAtt = @{"MyAttribute1"='MyValue1';"MyAttribute2"="MyValue2"}
+    $string = head {"woop"} -Attributes $CustomAtt -Style $Style -Class $class -id $id
+
+    $string = Get-Service | Where-Object {$_.Status -eq "running" -and $_.StartType -eq "Automatic"} | select DisplayName,Status,StartType -first 2 | ConvertTo-HTMLTable
+    
+    if($string -is [array]){
+        $string = $String -join "" 
+    }
+
+    it "Should contain opening and closing <table> tags" {
+        $string -match '^<table.*>' | should be $true
+        $string -match '.*</table>$' | should be $true
+        
+    }
+
+    it "Should contain opening and closing <thead> tags" {
+        $string -match '.*<thead.*>' | should be $true
+        $string -match '.*</thead>.*' | should be $true
+        
+    }
+
+    it "Should contain opening and closing <tr> tags" {
+        $string -match '.*<tr.*>' | should be $true
+        $string -match '.*</tr>.*' | should be $true
+        
+    }
+
+    <#
+    
+    it "Should contain 3 <tr> tags" {
+        $string -match '.*<tr.*>' | should be $true
+        $string -match '.*</tr>.*' | should be $true
+        
+    }
+    #>
+
+    it "Header should match DisplayName,StartType,Status"{
+        $string -match ".*<td>DisplayName</td><td>StartType</td><td>Status</td>.*" | should be $true
+    }
+    
+    it "Should contain opening and closing <td> tags" {
+        $string -match '.*<td.*>' | should be $true
+        $string -match '.*</td>.*' | should be $true
+        
+    }
+    
+
+    #Add test to count td (should be 3)
+    
+    
+}
+
+
+
+Pop-Location
