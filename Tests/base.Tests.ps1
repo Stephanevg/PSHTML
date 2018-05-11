3413123b9f7ef7aa62b6874dd0d6132a128806a5
stephanevg <svangulick@gmail.com>
2018-05-11 09:17:23 +0200

    v0.5.1: Fixed set-htmltag bugs

---
+++ Tests/base.Tests.ps1
@@ -0,0 +1,67 @@
+$TestsPath = Split-Path $MyInvocation.MyCommand.Path
+
+#$FunctionsPath = join-Path -Path (get-item $TestsPath).Parent -ChildPath "Functions"
+
+$RootFolder = (get-item $TestsPath).Parent
+
+Push-Location -Path $RootFolder.FullName
+
+set-location -Path $RootFolder.FullName
+
+Write-Verbose "Importing module"
+
+import-module .\PSHTML -Force
+
+    Describe "Testing base - ScriptBlock" {
+        
+
+        $Class = "MyClass"
+        $Id = "MyID"
+        $CustomAtt = @{"MyAttribute1"='MyValue1';"MyAttribute2"="MyValue2"}
+        
+        $string = base -href "www.powershelldistrict.com" -Target _top -Attributes $CustomAtt -Class $class -id $id
+       
+        if($string -is [array]){
+            $string = $String -join "" 
+        }
+
+        it "Should contain opening and closing tags" {
+            $string -match '^<base .*/>$' | should be $true
+            
+        }
+
+        it "Testing common parameters: href"{
+            $string -match '^<base.*href="www\.powershelldistrict\.com".*/>' | should be $true
+        }
+
+        it "Testing common parameters: Attributes should not be present"{
+            $string -match '^<base.*attributes=.*>' | should be $false
+        }
+
+        it "Testing common parameters: Target"{
+            $string -match '^<base.*target="_top".*>' | should be $true
+        }
+
+        it "Testing common parameters: Class"{
+            $string -match '^<base.*class="myclass".*>' | should be $true
+        }
+        it "Testing common parameters: ID"{
+            $string -match '^<base.*id="myid".*>' | should be $true
+        }
+
+        it "Testing Attributes parameters"{
+
+            foreach($at in $CustomAtt.Keys){
+                $val = $null
+                $val = $CustomAtt[$at]
+                $string -match "^<base.*$at=`"$val`".*>" | should be $true
+            }
+
+            
+        }
+
+        
+    }
+
+
+Pop-Location
