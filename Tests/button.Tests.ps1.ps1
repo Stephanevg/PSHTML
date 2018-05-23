e0d73e77556ba3f742574147720f41f92cc80e4e
stephanevg <svangulick@gmail.com>
2018-05-23 21:41:58 +0200

    v0.5.10: Added button tag

---
+++ Tests/button.Tests.ps1.ps1
@@ -0,0 +1,60 @@
+$TestsPath = Split-Path $MyInvocation.MyCommand.Path
+
+#$FunctionsPath = join-Path -Path (get-item $TestsPath).Parent -ChildPath "Functions"
+
+$RootFolder = (get-item $TestsPath).Parent
+
+Push-Location -Path $RootFolder.FullName
+
+set-location -Path $RootFolder.FullName
+
+Write-Verbose "Importing module"
+
+import-module .\PSHTML -Force
+    Describe "Testing button - ScriptBlock" {
+        
+
+        $Class = "MyClass"
+        $Id = "MyID"
+        $style = "Background:green"
+        $CustomAtt = @{"MyAttribute1"='MyValue1';"MyAttribute2"="MyValue2"}
+
+        $string = button {"woop"} -Attributes $CustomAtt -Class $class -id $id
+       
+        if($string -is [array]){
+            $string = $String -join "" 
+        }
+
+        it "Should contain opening and closing tags" {
+            $string -match '^<button.*>' | should be $true
+            $string -match '.*</button>$' | should be $true
+            
+        }
+
+        it "Testing content in child element"{
+            $string -match "^.*>woop<.*" | should be $true
+        }
+
+        it "Testing common parameters: Class"{
+            $string -match '^<button.*class="myclass".*>' | should be $true
+        }
+        it "Testing common parameters: ID"{
+            $string -match '^<button.*id="myid".*>' | should be $true
+        }
+
+        it "Testing Attributes parameters"{
+
+            foreach($at in $CustomAtt.Keys){
+                $val = $null
+                $val = $CustomAtt[$at]
+                $string -match "^<button.*$at=`"$val`".*>" | should be $true
+            }
+
+            
+        }
+
+        
+    }
+
+
+Pop-Location
