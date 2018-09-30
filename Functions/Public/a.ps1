ea12a385e5d8b4a99dadd6679879bd32c6efdfe7
stephanevg <svangulick@gmail.com>
2018-09-30 22:10:53 +0200

    updated <a> to version 2.0

--- Functions/Public/a.ps1
+++ Functions/Public/a.ps1
@@ -1,8 +1,18 @@
 Function a {
     <#
         .SYNOPSIS
-        Generates a a HTML tag.
+        Generates a <a> HTML tag.
         
+        .PARAMETER HREF
+            Specify where the link should point to.
+            
+        .PARAMETER TARGET
+        
+        Specify where the new page should open to.
+        
+        Should be one of the following values:
+        "_self","_blank","_parent","_top"
+
         .PARAMETER Class
         Allows to specify one (or more) class(es) to assign the html element.
         More then one class can be assigned by seperating them with a white space.
@@ -19,6 +29,7 @@ Function a {
 
         .EXAMPLE
         The following exapmles show cases how to create an empty a, with a class, an ID, and, custom attributes.
+        
         a -Class "myclass1 MyClass2" -Id myid -Attributes @{"custom1"='val1';custom2='val2'}
 
         Generates the following code:
@@ -28,8 +39,9 @@ Function a {
         
 
         .NOTES
-        Current version 1.0
+        Current version 2.0
         History:
+            2018.09.30;Stephanevg;Updated to version 2.0
             2018.04.10;Stephanevg; Added parameters
             2018.04.01;Stephanevg;Creation.
 
@@ -37,71 +49,59 @@ Function a {
 
     Param(
 
-        [Parameter(
-            ValueFromPipeline = $true,
-            Mandatory = $false,
-            Position = 0
-        )]
-        [scriptblock]
+        [Parameter(Mandatory=$false)]
+        [AllowEmptyString()]
+        [AllowNull()]
         $Content,
-
-        [Parameter(Position = 1)]
-        [String]$Class,
-
-        [Parameter(Position = 2)]
-        [String]$Id,
-
-        [Parameter(Position = 3)]
-        [String]$Style,
-
-        [Parameter(Position = 4)]
-        [Hashtable]$Attributes,
 
         [Parameter(Mandatory = $true)]
         [String]$href,
 
         [ValidateSet("_self","_blank","_parent","_top")]
-        [String]$Target = "_self"
+        [String]$Target = "_self",
+
+        [AllowEmptyString()]
+        [AllowNull()]
+        [String]$Class,
+
+        [String]$Id,
+        
+        [String]$Style,
+
+        [Hashtable]$Attributes
+
     )
     Process{
 
 
-        $attr = ""
-        $CommonParameters = ("Attributes", "Content") + [System.Management.Automation.PSCmdlet]::CommonParameters + [System.Management.Automation.PSCmdlet]::OptionalCommonParameters
+        
+        $CommonParameters = "tagname" + [System.Management.Automation.PSCmdlet]::CommonParameters + [System.Management.Automation.PSCmdlet]::OptionalCommonParameters
         $CustomParameters = $PSBoundParameters.Keys | ? { $_ -notin $CommonParameters }
         
+        $htmltagparams = @{}
+        $tagname = "a"
         if($CustomParameters){
             
             foreach ($entry in $CustomParameters){
 
+                if($entry -eq "content"){
+
+                    
+                    $htmltagparams.$entry = $PSBoundParameters[$entry]
+                }else{
+                    $htmltagparams.$entry = "{0}" -f $PSBoundParameters[$entry]
+                }
                 
-                $Attr += "{0}=`"{1}`" " -f $entry,$PSBoundParameters[$entry]
     
             }
-                
-        }
 
-        if($Attributes){
-            foreach($entry in $Attributes.Keys){
-               
-                $attr += "{0}=`"{1}`" " -f $entry,$Attributes[$Entry]
+            if($Attributes){
+                $htmltagparams += $Attributes
             }
+
+            Set-HtmlTag -TagName $tagname -Attributes $htmltagparams -TagType nonVoid   
         }
 
-        if($attr){
-            "<a {0} >"  -f $attr
-        }else{
-            "<a>"
-        }
-        
-      
-
-        if($Content){
-            $Content.Invoke()
-        }
-            
-
-        '</a>'
     }
     
     
