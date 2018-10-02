041ed18dc4f8bb9f48a2a97ac4d16937ddbed05a
Kevin Bates <kevin96bates@gmail.com>
2018-10-02 22:45:28 +0200

    updated figcaption finction

--- Functions/Public/figcaption.ps1
+++ Functions/Public/figcaption.ps1
@@ -16,9 +16,10 @@ Function figcaption {
     figcaption "woop3" -Class "class" -Id "something" -Style "color:red;"
 
     .NOTES
-    Current version 1.0
+    Current version 2.0
        History:
-           2018.04.01;bateskevinhanevg;Creation.
+           2018.10.02;bateskevin;Updated to v2.
+           2018.04.01;bateskevin;Creation.
     .LINK
         https://github.com/Stephanevg/PSHTML
     #>
@@ -45,41 +46,36 @@ Function figcaption {
         [Hashtable]$Attributes
     )
 
-        $attr = ""
-        $CommonParameters = ("Attributes", "content") + [System.Management.Automation.PSCmdlet]::CommonParameters + [System.Management.Automation.PSCmdlet]::OptionalCommonParameters
-        $CustomParameters = $PSBoundParameters.Keys | Where-Object -FilterScript { $_ -notin $CommonParameters }
+    Process {
+
+        $CommonParameters = "tagname" + [System.Management.Automation.PSCmdlet]::CommonParameters + [System.Management.Automation.PSCmdlet]::OptionalCommonParameters
+        $CustomParameters = $PSBoundParameters.Keys | ? { $_ -notin $CommonParameters }
+        
+        $htmltagparams = @{}
+        $tagname = "figcaption"
 
         if($CustomParameters){
 
             foreach ($entry in $CustomParameters){
 
 
-                $Attr += "{0}=`"{1}`" " -f $entry,$PSBoundParameters[$entry]
+                if($entry -eq "content"){
 
+                    
+                    $htmltagparams.$entry = $PSBoundParameters[$entry]
+                }else{
+                    $htmltagparams.$entry = "{0}" -f $PSBoundParameters[$entry]
+                }
+                
+    
             }
 
-        }
-
-        if($Attributes){
-            foreach($entry in $Attributes.Keys){
-
-                $attr += "{0}=`"{1}`" " -f $entry,$Attributes[$Entry]
+            if($Attributes){
+                $htmltagparams += $Attributes
             }
+
+
+            Set-HtmlTag -TagName $tagname -Attributes $htmltagparams -TagType NonVoid   
         }
-
-
-if($attr){
-    $return = @"
-    <figcaption $attr>$Content</figcaption>
-"@
-
-}else{
-
-    $return =     @"
-    <figcaption>$Content</figcaption>
-"@
-}
-
-return $return
-
+    }
 }
