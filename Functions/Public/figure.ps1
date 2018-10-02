f51d196e489f89728da4d782a0342ba47f065dda
Kevin Bates <kevin96bates@gmail.com>
2018-10-02 22:47:49 +0200

    updated versionnumber

--- Functions/Public/figure.ps1
+++ Functions/Public/figure.ps1
@@ -14,7 +14,7 @@ Function figure {
 
 
         .NOTES
-        Current version 1.0
+        Current version 2.0
         History:
            2018.10.02;bateskevin;Updated to v2.
            2018.04.01;bateskevin;Creation.
