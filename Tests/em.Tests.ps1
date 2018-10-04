31401b3a4fe12c6dd6f5eda0137a2d6691c3c2ba
Andrew Wickham <andrew@awickham.com>
2018-10-04 14:26:51 -0400

    Added em tag

---
+++ Tests/em.Tests.ps1
@@ -0,0 +1,66 @@
+$TestsPath = Split-Path $MyInvocation.MyCommand.Path
+
+#$FunctionsPath = join-Path -Path (get-item $TestsPath).Parent -ChildPath "Functions"
+
+$RootFolder = (get-item $TestsPath).Parent
+
+Push-Location -Path $RootFolder.FullName
+
+set-location -Path $RootFolder.FullName
+
+Write-Verbose "Importing module"
+
+import-module .\PSHTML -Force
+
+Context "Testing PSHTML"{
+    Describe "Testing em" {
+
+
+        $Class = "MyClass"
+        $Id = "MyID"
+        $Style = "Background:green"
+        $CustomAtt = @{"MyAttribute1"='MyValue1';"MyAttribute2"="MyValue2"}
+        $string = em {"woop"} -Attributes $CustomAtt -Style $Style -Class $class -id $id
+
+        if($string -is [array]){
+            $string = $String -join ""
+        }
+
+        it "Should contain opening and closing tags" {
+            $string -match '^<em.*>' | should be $true
+            $string -match '.*</em>$' | should be $true
+
+        }
+
+        it "Testing content in child element"{
+            $string -match "^.*>woop<.*" | should be $true
+        }
+
+        it "Testing common parameters: Class"{
+            $string -match '^<em.*class="myclass".*>' | should be $true
+        }
+        it "Testing common parameters: ID"{
+            $string -match '^<em.*id="myid".*>' | should be $true
+        }
+        it "Testing common parameters: Style"{
+            $string -match '^<em.*style=".+".*>' | should be $true
+        }
+
+
+        it "Testing Attributes parameters"{
+
+            foreach($at in $CustomAtt.Keys){
+                $val = $null
+                $val = $CustomAtt[$at]
+                $string -match "^<em.*$at=`"$val`".*>" | should be $true
+            }
+
+
+        }
+
+
+    }
+
+}
+
+Pop-Location
