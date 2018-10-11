292fc3feac3f5ba9bf63110e80f792ec9775f3f4
NicolasB <b.nicolas.49@gmail.com>
2018-10-11 09:12:30 +0200

    Add pester test to check blockquote function with pipeline

--- Tests/blockquote.tests.ps1
+++ Tests/blockquote.tests.ps1
@@ -60,6 +60,52 @@ Context "Testing PSHTML"{
 
     }
 
+    Describe "Testing blockquote with Pipeline" {
+
+
+        $Class = "MyClass"
+        $Id = "MyID"
+        $Style = "Background:green"
+        $CustomAtt = @{"MyAttribute1" = 'MyValue1'; "MyAttribute2" = "MyValue2"}
+        $string = p{} | blockquote -Attributes $CustomAtt -Style $Style -Class $class -id $id
+
+        if ($string -is [array]) {
+            $string = $String -join ""
+        }
+
+        it "Should contain opening and closing tags" {
+            $string -match '^<blockquote.*>' | should be $true
+            $string -match '.*</blockquote>$' | should be $true
+
+        }
+
+        it "Testing content p{} in child element" {
+            $string -match "^.*<p></p>.*" | should be $true
+        }
+
+        it "Testing common parameters: Class" {
+            $string -match '^<blockquote.*class="myclass".*>' | should be $true
+        }
+        it "Testing common parameters: ID" {
+            $string -match '^<blockquote.*id="myid".*>' | should be $true
+        }
+        it "Testing common parameters: Style" {
+            $string -match '^<blockquote.*style=".+".*>' | should be $true
+        }
+
+        it "Testing Attributes parameters" {
+
+            foreach ($at in $CustomAtt.Keys) {
+                $val = $null
+                $val = $CustomAtt[$at]
+                $string -match "^<blockquote.*$at=`"$val`".*>" | should be $true
+            }
+
+
+        }
+
+
+    }
 }
 
 Pop-Location
