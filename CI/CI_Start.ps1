3207fad6bc7f2015703ffd5ff3e30591770e45db
stephanevg <svangulick@gmail.com>
2018-10-16 00:35:15 +0200

    push psgallery

--- CI/CI_Start.ps1
+++ CI/CI_Start.ps1
@@ -1,7 +1,13 @@
 import-module pester
 start-sleep -seconds 2
 
-$moduleName = "pshtml"
+write-output "BUILD_FOLDER: $($env:APPVEYOR_BUILD_FOLDER)"
+write-output "PROJECT_NAME: $($env:APPVEYOR_PROJECT_NAME)"
+write-output "BRANCH: $($env:APPVEYOR_REPO_BRANCH)"
+$ModuleClonePath = Join-Path -Path $env:APPVEYOR_BUILD_FOLDER -ChildPath $env:APPVEYOR_PROJECT_NAME
+Write-Output "MODULE CLONE PATH: $($ModuleClonePath)"
+
+$moduleName = "$($env:APPVEYOR_PROJECT_NAME)"
 Get-Module $moduleName
 
 #Pester Tests
@@ -16,9 +22,21 @@ $res = Invoke-Pester -Path ".\Tests" -OutputFormat NUnitXml -OutputFile TestsRes
 if ($res.FailedCount -gt 0) { throw "$($res.FailedCount) tests failed."};
 
     
-    if($res.FailedCount -eq 0 -and $env:APPVEYOR_REPO_BRANCH -eq "master" ){
-        import-module $ModuleName -force
-        $GalleryVersion = (Find-Module $ModuleName).version
+if ($res.FailedCount -eq 0 -and $res.successcount -ne 0) {
+    If ($env:APPVEYOR_REPO_BRANCH -eq "master") {
+        Write-host "[$($env:APPVEYOR_REPO_BRANCH)] All tested Passed, and on Branch 'master'"
+        $OfficialModulePath = $env:PSModulePath.SPlit(";")[0]
+
+        Copy-Item -Path $ModuleClonePath -Destination $OfficialModulePath -Recurse -Force
+
+        Write-host "[$($env:APPVEYOR_REPO_BRANCH)][$($ModuleName)] Import module from: $($ModuleClonePath)\$($ModuleName).psd1" -ForegroundColor DarkGreen
+        import-module "$($ModuleClonePath)\$($ModuleName).psd1" -Force
+        try{
+            $GalleryModule = Find-Module $ModuleName -ErrorAction stop
+            $GalleryVersion = $GalleryModule.version 
+        }catch{
+            Write-host "[$($env:APPVEYOR_REPO_BRANCH)][$($ModuleName)] Module not found on the gallery (is this the First deployment perhaps?)"
+        }
         $LocalVersion = (get-module $ModuleName).version.ToString()
         if($Localversion -le $GalleryVersion){
             Write-host "[$($env:APPVEYOR_REPO_BRANCH)] PsClassUtils version $($localversion)  is identical with the one on the gallery. No upload will be done."
