213ad7f21649a55bc853cd52003456b77924012e
stephanevg <svangulick@gmail.com>
2018-10-17 21:46:41 +0200

    v0.5.20: updated Install-PSHTMLVSCodeSnippets Added -force

--- PSHTML/Functions/Public/Install-PSHTMLVSCodeSnippets.ps1
+++ PSHTML/Functions/Public/Install-PSHTMLVSCodeSnippets.ps1
@@ -1,27 +1,58 @@
 function Install-PSHTMLVSCodeSnippets {
-<#
-.SYNOPSIS
-    Copy the PSHTML VSCode Snippets to the right location
-.DESCRIPTION
-    Gets the PSHTML VSCode snippet files and copies them to users appdata folder
-.NOTES
-        Current version 0.1
-        History:
-        2018.10.16;FishFenly;Creation.
-#>
+    <#
+    .SYNOPSIS
+        Copy the PSHTML VSCode Snippets to the right location
+    .DESCRIPTION
+        Gets the PSHTML VSCode snippet files and copies them to users appdata folder
+
+    .EXAMPLE
+
+    .EXAMPLE
+
+
+
+    .NOTES
+            Current version 1.0
+            History:
+            2018.10.17;stephanev;updated to v 1.0 - added error handling, and -force parameter
+            2018.10.16;FishFenly;Creation.
+    #>
+    [CmdletBinding()]
+    Param(
+        [Switch]$Force
+    )
+    
 
     $callstack = Get-PSCallStack | Select-Object -ExpandProperty scriptname
     
-    $path = Split-path -path (
-        Split-path -Path (
-            Split-Path -path $callstack -Parent) -Parent) -Parent
+    $path = Split-path -path (Split-path -Path (Split-Path -path $callstack -Parent) -Parent) -Parent
 
     $snippetsfolder = join-path $path -ChildPath "Snippets"
 
+    $AllSnipets = Get-childItem -path $snippetsfolder
 
-    foreach ($snippet in (Get-Item $snippetsfolder\*.code-snippets | Select-Object -ExpandProperty FullName)) {
-            Write-Verbose "Copying $snippet to $($env:APPDATA)\Code\User\snippets"
-            Copy-Item -Path $snippet `
-                    -Destination "$($env:APPDATA)\Code\User\snippets" -Force
+    $Paras = @{}
+    
+    $Paras.Destination = "$($env:APPDATA)\Code\User\snippets"
+    $Paras.errorAction =  "Stop"
+
+    if($Force){
+        $Paras.Force = $true
+    }
+
+    if($AllSnipets){
+        foreach ($snippet in $AllSnipets) {
+            $Paras.Path = $null
+            $Paras.Path = $snippet.FullName 
+            Write-Verbose "Copying $($snippet.FullName) to $($env:APPDATA)\Code\User\snippets"
+            try {
+                Copy-Item @Paras
+            }Catch{
+                
+                    Write-warning "$_"
+            }
+        }
+    }else{
+        write-warning "No snippts found in $SnippetsFolder"
     }
 }
