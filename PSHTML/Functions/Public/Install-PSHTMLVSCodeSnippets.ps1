fae44d91b7fb22584bbd66ec39c67927418d42ca
stephanevg <svangulick@gmail.com>
2018-10-21 23:57:57 +0200

    Added Pester tests Install-PSHTMLVSCodeSnippets

--- PSHTML/Functions/Public/Install-PSHTMLVSCodeSnippets.ps1
+++ PSHTML/Functions/Public/Install-PSHTMLVSCodeSnippets.ps1
@@ -14,26 +14,28 @@ function Install-PSHTMLVSCodeSnippets {
     .NOTES
             Current version 1.0
             History:
+            2018.10.21;Stephanevg;Fix wrong path due to module restrucutre.
             2018.10.17;stephanev;updated to v 1.0 - added error handling, and -force parameter
             2018.10.16;FishFenly;Creation.
     #>
     [CmdletBinding()]
     Param(
-        [String]$DestinationPath = "$($env:APPDATA)\Code\User\snippets",
+        [String]$Path = "$($env:APPDATA)\Code\User\snippets",
         [Switch]$Force
     )
     
 
     $callstack = Get-PSCallStack | Select-Object -ExpandProperty scriptname
     
-    $Rootpath = Split-path -path (Split-path -Path (Split-Path -path $callstack -Parent) -Parent) -Parent
+    $Rootpath = Split-path -path (Split-Path -path $callstack -Parent)-Parent
 
+    $Rootpath = split-Path -Path $PSScriptRoot -Parent
     $snippetsfolder = join-path $Rootpath -ChildPath "Snippets"
 
     $AllSnipets = Get-childItem -path $snippetsfolder
 
     $Paras = @{}
-    $Paras.Destination = $DestinationPath
+    $Paras.Destination = $Path
     $Paras.errorAction =  "Stop"
 
     if($Force){
@@ -44,7 +46,7 @@ function Install-PSHTMLVSCodeSnippets {
         foreach ($snippet in $AllSnipets) {
             $Paras.Path = $null
             $Paras.Path = $snippet.FullName 
-            Write-Verbose "Copying $($snippet.FullName) to $($env:APPDATA)\Code\User\snippets"
+            Write-Verbose "Copying $($snippet.FullName) to $($Paras.Destination)"
             try {
                 Copy-Item @Paras
             }Catch{
--- Tests/Install-PSHTMLVSCodeSnippets.tests.ps1
+++ Tests/Install-PSHTMLVSCodeSnippets.tests.ps1
@@ -1,13 +1,43 @@
-Describe "VSCode Snippets store with PSHTML Snippets" {
-    Context "Testing PSHTML Snippets exist" {
-        It "has the Small snippet" {
-            Test-Path "$($env:APPDATA)\Code\User\snippets\PsHtml_BoilerPlate_small.Snippet.code-snippets" | Should Be $true
+$TestsPath = Split-Path $MyInvocation.MyCommand.Path
+
+$RootFolder = (get-item $TestsPath).Parent
+
+Push-Location -Path $RootFolder.FullName
+
+set-location -Path $RootFolder.FullName
+
+Write-Verbose "Importing module"
+
+import-module .\PSHTML -Force
+
+
+Describe "Testing Install-VSCodeSnippets" {
+
+    $SnippetTypes = @("small","medium","full")
+
+    it "The cmdlet should not throw"{
+        {install-PSHTMLVsCodeSnippets} | should not throw
+    }
+
+    $Items = gci "$($env:APPDATA)\Code\User\snippets\"
+    it "Should create the snippets at correct path: $($env:APPDATA)\Code\User\snippets\"{
+        $ITems | should not benullOrEmpty
+        
+    }
+
+    It "Should contain 3 snippets"{
+
+        ($Items | measure).Count | should be 3
+    }
+
+    foreach($SnippetType in $SnippetTypes){
+
+        It "Should contain snippet -> $($SnippetType)" {
+            $items | ? {$_.Name -like "*$SnippetType*"} | should not benullOrEmpty
         }
-        It "has the Medium snippet" {
-            Test-Path "$($env:APPDATA)\Code\User\snippets\PsHtml_BoilerPlate_small.Snippet.code-snippets" | Should Be $true
-        }
-        It "has the Full snippet" {
-            Test-Path "$($env:APPDATA)\Code\User\snippets\PsHtml_BoilerPlate_small.Snippet.code-snippets" | Should Be $true
-        } 
     }
 }
+
+
+
+Pop-Location
