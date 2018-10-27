306327ee2af07ba49b489e87d2fb33e6ee3c3d1b
rchaganti <Ravikanth@Ravichagant.com>
2018-10-27 11:57:59 +0530

    adding br tag

--- Functions/Public/br.ps1
+++ Functions/Public/br.ps1
@@ -1,87 +1,26 @@
-Function br {
+Function br
+{
     <#
     .SYNOPSIS
-    Create a h1 title in an HTML document.
+    Create a br in an HTML document.
 
     .EXAMPLE
 
-    h1
-    .EXAMPLE
-    h1 "woop1" -Class "class"
-
-    .EXAMPLE
-    h1 "woop2" -Class "class" -Id "MainTitle"
-
-    .EXAMPLE
-    h1 {"woop3"} -Class "class" -Id "MaintTitle" -Style "color:red;"
-
+    br
+    
     .Notes
-    Author: Stéphane van Gulick
-    Version: 1.0.0
+    Author: Ravikanth Chaganti
+    Version: 1.0.0.0
     History:
-        2018.04.08;Stephanevg; Updated to version 1.0: Updated content block to support string & ScriptBlock
-        2018.04.08;Stephanevg; Fixed custom Attributes display bug. Updated help
-        2018.03.25;@Stephanevg; Added Styles, ID, CLASS attributes functionality
-        2018.03.25;@Stephanevg; Creation
+        2018.10.27;rchaganti; Adding br element
     .LINK
         https://github.com/Stephanevg/PSHTML
     #>
     [Cmdletbinding()]
-    Param(
-        [Parameter(Mandatory=$false)]
-        [AllowEmptyString()]
-        [AllowNull()]
-        $Content,
+    param
+    (
 
-        [AllowEmptyString()]
-        [AllowNull()]
-        [String]$Class,
-
-        [String]$Id,
-
-        [AllowEmptyString()]
-        [AllowNull()]
-        [String]$Style,
-
-        [Hashtable]$Attributes
     )
 
-    $attr = ""
-    $CommonParameters = ("Attributes", "Content") + [System.Management.Automation.PSCmdlet]::CommonParameters + [System.Management.Automation.PSCmdlet]::OptionalCommonParameters
-    $CustomParameters = $PSBoundParameters.Keys | Where-Object -FilterScript { $_ -notin $CommonParameters }
-
-    if($CustomParameters){
-
-        foreach ($entry in $CustomParameters){
-
-
-            $Attr += "{0}=`"{1}`" " -f $entry,$PSBoundParameters[$entry]
-
-        }
-
-    }
-
-    if($Attributes){
-        foreach($entry in $Attributes.Keys){
-
-            $attr += "{0}=`"{1}`" " -f $entry,$Attributes[$Entry]
-        }
-    }
-
-    if($attr){
-        "<br {0} >"  -f $attr
-    }else{
-        "<br>"
-    }
-
-
-    if($Content){
-
-        if($Content -is [System.Management.Automation.ScriptBlock]){
-            $Content.Invoke()
-        }else{
-            $Content
-        }
-    }
-
+    return '<br>'
 }
