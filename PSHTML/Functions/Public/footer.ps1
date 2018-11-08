c83df4cecae7a33cf57fc6c2ce815d1a28a3c8cc
stephanevg <svangulick@gmail.com>
2018-11-08 23:53:34 +0100

    footer 3.1

--- PSHTML/Functions/Public/footer.ps1
+++ PSHTML/Functions/Public/footer.ps1
@@ -35,7 +35,7 @@ Function Footer {
 
 
         .NOTES
-        Current version 1.0
+        Current version 3.1
         History:
         2018.10.30;@ChristopheKumor;Updated to version 3.0
             2018.04.10;Stephanevg; Added parameters
@@ -66,7 +66,7 @@ Function Footer {
 
     Process {
         $tagname = "footer"
-        Set-HtmlTag -TagName $tagname -PSBParameters $PSBoundParameters -MyCParametersKeys $MyInvocation.MyCommand.Parameters.Keys -TagType nonVoid
+        Set-HtmlTag -TagName $tagname -Parameters $PSBoundParameters -TagType nonVoid
     }
 
 }
