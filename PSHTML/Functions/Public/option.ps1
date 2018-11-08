b4042b3fef1a28ad59522fa6104ee7e1d0e62173
stephanevg <svangulick@gmail.com>
2018-11-08 23:27:36 +0100

    option 3.1

--- PSHTML/Functions/Public/option.ps1
+++ PSHTML/Functions/Public/option.ps1
@@ -31,7 +31,7 @@ Function option {
     
 
     .NOTES
-    Current version 2.0
+    Current version 3.1
        History:
        2018.10.30;@ChristopheKumor;Updated to version 3.0
             2018.10.05;@stephanevg;Creation.
@@ -71,7 +71,7 @@ Function option {
 
     $tagname = "option"
  
-    Set-HtmlTag -TagName $tagname -PSBParameters $PSBoundParameters -MyCParametersKeys $MyInvocation.MyCommand.Parameters.Keys -TagType NonVoid
+    Set-HtmlTag -TagName $tagname -Parameters $PSBoundParameters -TagType NonVoid
 
 }
 
