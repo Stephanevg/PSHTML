14d37ee2eca4e7972c858a61f87c9b8216241790
stephanevg <svangulick@gmail.com>
2018-11-09 00:03:08 +0100

    html 3.1

--- PSHTML/Functions/Public/html.ps1
+++ PSHTML/Functions/Public/html.ps1
@@ -11,6 +11,8 @@ Function html {
 
     The <html> tag is the container for all other HTML elements (except for the <!DOCTYPE> tag).
 
+    Detailed information can be found here --> https://www.w3schools.com/tags/tag_html.asp
+
     .PARAMETER Class
     Allows to specify one (or more) class(es) to assign the html element.
     More then one class can be assigned by seperating them with a white space.
@@ -29,7 +31,7 @@ Function html {
 
 
     .NOTES
-    Current version 1.1.0
+    Current version 3.1.0
     History:
     2018.10.30;@ChristopheKumor;Updated to version 3.0
         2018.04.10;Stephanevg; Added parameters
@@ -65,6 +67,6 @@ Function html {
 
     Process {
         $tagname = "html"
-        Set-HtmlTag -TagName $tagname -PSBParameters $PSBoundParameters -MyCParametersKeys $MyInvocation.MyCommand.Parameters.Keys -TagType nonVoid
+        Set-HtmlTag -TagName $tagname -Parameters $PSBoundParameters -TagType nonVoid
     }
 }
