26887f369ad3832bbb39b6450efbe263fabda7f9
stephanevg <svangulick@gmail.com>
2018-11-08 23:45:15 +0100

    figure 3.1

--- PSHTML/Functions/Public/figure.ps1
+++ PSHTML/Functions/Public/figure.ps1
@@ -14,7 +14,7 @@ Function figure {
 
 
         .NOTES
-        Current version 1.0
+        Current version 3.1
         History:
         2018.10.30;@ChristopheKumor;Updated to version 3.0
            2018.10.02;bateskevin;Updated to v2.
@@ -30,7 +30,6 @@ Function figure {
             Mandatory = $false,
             Position = 0
         )]
-        [scriptblock]
         $Content,
 
         [Parameter(Position = 1)]
@@ -49,7 +48,7 @@ Function figure {
 
         $tagname = "figure"
 
-        Set-HtmlTag -TagName $tagname -PSBParameters $PSBoundParameters -MyCParametersKeys $MyInvocation.MyCommand.Parameters.Keys -TagType nonVoid
+        Set-HtmlTag -TagName $tagname -Parameters $PSBoundParameters -TagType nonVoid
         
     }
 }
