dde3d8f0a6eff38fe21e12f7521df42fdadf7e50
stephanevg <svangulick@gmail.com>
2018-11-09 23:36:53 +0100

    Generated Graph. Doesn't work (yet)

--- PSHTML/Classes/Public/Chart.ps1
+++ PSHTML/Classes/Public/Chart.ps1
@@ -1,3 +1,4 @@
+
 Enum ChartType {
     bar
     line
@@ -17,12 +18,40 @@ Class Color {
     static [string] $Green = "rgb(173,255,47)"
     static [string] $Orange = "rgb(255,165,0)"
 }
-Class ChartData {
-
-}
 
 Class ChartOption {
 
+}
+
+Class ChartDataSets {
+    [System.Collections.ArrayList] $Data = @()
+    [String]$Label
+    [String] $xAxisID
+    [String] $yAxisID
+    [String[]]  $BackgroundColor = @()
+    [String[]]  $Bordercolor = @()
+    [int]    $BorderWidth
+    [String] $BorderSkipped
+    [Color]  $HoverBackGroundColor
+    [Color]  $hoverBorderColor
+    [int]    $hoverBorderWidth
+}
+Class ChartData {
+    [System.Collections.ArrayList] $Labels = @()
+    [ChartDataSets] $DataSets = [ChartDataSets]::New()
+}
+
+Class BarData : ChartData {
+    
+    
+}
+
+Class BarOptions : ChartOption {
+    [int]$BarPercentage = 0.9
+    [Int]$CategoryPercentage = 0.8
+    [String]$BarThickness
+    [Int]$MaxBarThickness
+    [Bool] $OffSetGridLines = $true
 }
 
 Class BaseChart {
@@ -55,17 +84,16 @@ Class BaseChart {
         #Do stuff with $DEfinition
     }
 
-    Hidden [String]GetDefinitionStart(){
-        Return @"
-        var ctx = document.getElementById("myChart").getContext('2d');
-        var myChart = new Chart(ctx, {
+    Hidden [String]GetDefinitionStart([String]$CanvasID){
+        $Start = @"
+var ctx = document.getElementById("$($CanvasID)").getContext('2d');
+var myChart = new Chart(ctx, 
 "@
+    return $Start
     }
 
     Hidden [String]GetDefinitionEnd(){
-        Return @"
-    });
-"@
+        Return ");"
     }
 
     Hidden [String]GetDefinitionBody(){
@@ -85,60 +113,22 @@ Class BaseChart {
                     #yAxes []
                         #Ticks
                             #BeginAtZero [bool]
-        
-        $Body = @"
-"@
+                            
+        $Body = $this | select @{N='Type';E={$_.Type.ToString()}},Data,Options  | convertto-Json -Depth 4
         Return $Body
     }
 
-    [String] GetDefinition(){
-        $this.CalculateDefinition
-        Return $this.Definition | convertto-Json
+    [String] GetDefinition([String]$CanvasID){
+        
+        $FullDefintion = [System.Text.StringBuilder]::new()
+        $FullDefintion.Append($this.GetDefinitionStart([String]$CanvasID))
+        $FullDefintion.AppendLine($this.GetDefinitionBody())
+        $FullDefintion.AppendLine($this.GetDefinitionEnd())
+        return $FullDefintion
     }
 }
 
 
-Class PieProperties : ChartData {
-    [Color]$BackGroundColor
-    [Color]$BorderColor
-    [int]$BorderWidth
-    [Color]$HoverBackGroundColor
-    [Color]$hoverBorderColor
-    [int]$hoverBorderWidth
-}
-
-Class PieChart : Basechart{
-
-    $Type = [ChartType]::Pie
-
-    PieChart([ChartData]$Data,[ChartOption]$Options){
-        $this.Data = $Data
-        $This.Options = $Options
-    }
-
-    
-}
-
-Class BarData : ChartData {
-    [System.Collections.ArrayList] $Label = @()
-    [String] $xAxisID
-    [String] $yAxisID
-    [Color]  $BackgroundColor
-    [Color]  $bordercolor
-    [int]    $BorderWidth
-    [String] $BorderSkipped
-    [Color]  $HoverBackGroundColor
-    [Color]  $hoverBorderColor
-    [int]    $hoverBorderWidth
-}
-
-Class BarOptions : ChartOption {
-    [int]$BarPercentage = 0.9
-    [Int]$CategoryPercentage = 0.8
-    [String]$BarThickness
-    [Int]$MaxBarThickness
-    [Bool] $OffSetGridLines = $true
-}
 
 Class BarChart : BaseChart{
 
@@ -155,6 +145,29 @@ Class BarChart : BaseChart{
     }
 
 }
+
+Class PieData : ChartData {
+    [Color]$BackGroundColor
+    [Color]$BorderColor
+    [int]$BorderWidth
+    [Color]$HoverBackGroundColor
+    [Color]$hoverBorderColor
+    [int]$hoverBorderWidth
+}
+
+Class PieChart : BaseChart{
+
+    $Type = [ChartType]::Pie
+
+    PieChart([ChartData]$Data,[ChartOption]$Options){
+        $this.Data = $Data
+        $This.Options = $Options
+    }
+
+    
+}
+
+
 
 ##Structure
     #Type
@@ -176,4 +189,44 @@ $Baroptions = [BarOptions]::New()
 $BarData = [BarData]::New()
 
 $BarChart = [BarChart]::New($BarData,$Baroptions)
-$BarChart.Data.Label.add("eee")
+$BarChart.Data.DataSets.Data.add(1)
+$BarChart.Data.DataSets.Data.add(32)
+$BarChart.Data.datasets.Data.add(24)
+$BarChart.Data.DataSets.Label = "Salutions, this is my graph!"
+
+$BarChart.Data.DataSets.BackgroundColor += [Color]::Orange
+$BarChart.Data.DataSets.BackgroundColor += [Color]::Green
+$BarChart.Data.DataSets.BackgroundColor += [Color]::blue
+
+$BarChart.Data.DataSets.Bordercolor += [Color]::blue
+$BarChart.Data.DataSets.Bordercolor += [Color]::Yellow
+$BarChart.Data.DataSets.Bordercolor += [Color]::Red
+$BarChart.Data.DataSets.BorderWidth = 1
+
+$BarChart.Data.Labels.Add("Label1")
+$BarChart.Data.Labels.Add("Label2")
+$BarChart.Data.Labels.Add("Label3")
+
+$CanvasID = "CanvasPlop"
+$HTMLPage = html {
+    head {
+        title 'Chart JS Demonstration'
+        
+    }
+    body {
+        
+         canvas -Height 500px -Width 500px -Id $CanvasID {
+
+         }
+         script -src "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js" -type "text/javascript"
+         script -content {
+            $BarChart.GetDefinition($CanvasID)
+         }
+         
+    }
+}
+
+
+$OutPath = "C:\Users\taavast3\OneDrive\Repo\Projects\OpenSource\PSHTML\PSHTML\Assets\Charts\GraphCode.html"
+$HTMLPage | out-file -FilePath $OutPath -Encoding utf8
+start $outpath
