7e1053235e27745ce636f40a0bcc0b755f437c00
stephanevg <svangulick@gmail.com>
2019-01-17 23:57:13 +0100

    updated CI, psm1 and config file.

--- PSHTML/PSHTML.psm1
+++ PSHTML/PSHTML.psm1
@@ -1,4 +1,4 @@
-﻿#Generated at 01/14/2019 07:24:38 by Stephane van Gulick
+﻿#Generated at 01/17/2019 23:54:34 by Stephane van Gulick
 
 Class ConfigurationFile {
 
@@ -22,8 +22,8 @@ Class ConfigurationFile {
         $this.LoadConfigurationData()
     }
 
-    [ConfigurationLog]GetLogConfig(){
-        return $this.Data.Logging
+    [ConfigurationLog]GetConfigurationLog(){
+        return $this.Data.GetConfigurationLog()
     }
 
     [ConfigurationAssets]GetAssetsConfig(){
@@ -39,18 +39,62 @@ Class ConfigurationFile {
     [String]GetAsset($Name){
         return $this.Data.Assets.Assets.$($Name)
     }
+
+    [Void]SetLogConfig([ConfigurationLog]$LogDocument){
+        $this.Data.SetConfigurationLog($LogDocument)
+    }
+
+    [String]GetLogfilePath(){
+        Return $this.Data.GetLogFilePath()
+    }
+    
+
+    
 }
 
 
 Class ConfigurationLog {
     [System.IO.FileInfo]$Path
-    [int]$MaxFiles
-    $MaxTotalSize
+    [int]$MaxFiles = 200
+    $MaxTotalSize = 5
+    hidden $Logfilename = "PSHTML.Log"
+
+    ConfigurationLog(){
+        $DefPath = $this.GetDefaultLogFolderPath()
+        $This.Path = $this.NewLogFile($DefPath)
+
+    }
+
+    ConfigurationLog ([System.IO.FileInfo]$Path){
+        $this.Path = $Path
+    }
+
+    ConfigurationLog ([System.IO.DirectoryInfo]$Path){
+        $this.Path = $this.NewLogFile($Path)
+    }
 
     ConfigurationLog([System.IO.FileInfo]$Path,[int]$Maxfiles,$MaxTotalSize){
         $this.Path = $Path
         $this.MaxFiles = $Maxfiles
         $this.MaxTotalSize = $MaxTotalSize
+    }
+
+    [System.IO.FileInfo] Hidden NewLogFile([System.IO.DirectoryInfo]$Directory){
+
+        Return [System.IO.FileInfo]([System.io.Path]::Combine($Directory.FullName,$this.Logfilename))
+    }
+
+    [String]GetLogfilePath(){
+        Return $This.Path.FullName
+    }
+
+    [String]GetDefaultLogFolderPath(){
+        if($global:IsLinux){
+            $p = "/var/log/pshtml/"
+        }Else{
+            $p = Join-Path $Env:Temp -ChildPath "pshtml"
+        }
+        return $p
     }
 }
 
@@ -91,7 +135,25 @@ Class PSHTMLConfiguration{
 
     PSHTMLConfiguration([System.IO.FileInfo]$Path){
         $json = (gc -Path $Path | ConvertFrom-Json)
-        $this.Logging = [ConfigurationLog]::New($json.Logging.Path,$json.Logging.MaxFiles,$json.Logging.MaxTotalSize)
+        if($json.Logging.Path -Eq 'default' -or $json.logging.Path -eq ""){
+            
+                if($global:IsLinux){
+                    $LogPath = "/var/log/pshtml/pshtml.log"
+                }Else{
+                    $LogPath = Join-Path $Env:ProgramData -ChildPath "pshtml/pshtml.log"
+                }
+
+
+
+        }else{
+            $P = Join-Path $json.logging.Path -ChildPath 'pshtml.log'
+            #$pa = [System.IO.Path]::Combine($P.FullName,'pshtml.log')
+            $LogPath = $P
+        }
+
+
+
+        $this.Logging = [ConfigurationLog]::New($LogPath,$json.Logging.MaxFiles,$json.Logging.MaxTotalSize)
         if($json.Assets.Path.Tolower() -eq 'default' -or $json.Assets.Path -eq '' ){
             $root = $Path.Directory.FullName
             $AssetsPath = "$Root/Assets"
@@ -102,12 +164,53 @@ Class PSHTMLConfiguration{
         $this.General = [ConfigurationGeneral]::New($Json.Configuration.Verbosity)
     }
 
+    [ConfigurationLog] GetConfigurationLog(){
+        return $this.Logging
+    }
 
+    [ConfigurationGeneral] GetConfigurationGeneral(){
+        return $this.General
+    }
+
+    [ConfigurationAssets] GetConfigurationAssets(){
+        return $this.Assets
+    }
+
+    SetConfigurationLog([ConfigurationLog]$LogDocument){
+        $this.Logging = $LogDocument
+    }
    
+    [String]GetLogfilePath(){
+        return $this.Logging.GetLogfilePath()
+    }
 
+    [String]GetDefaultLogFolderPath(){
+        return $this.Logging.GetDefaultLogFolderPath()
+    }
 }
 
+function New-Logfile {
+    [CmdletBinding()]
+    param (
+        
+        $Path = {Throw "Path parameter is mandatory."}
+    )
+    
+    begin {
+    }
+    
+    process {
+        if($Path -is [System.IO.DirectoryInfo] -or $Path -is [System.IO.fileInfo]){
 
+            Return [ConfigurationLog]::New($Path)
+        }Else{
+            Throw "Log file is of wrong type. Please specify a System.IO.DirectoryInfo or System.IO.fileIno type."
+        }
+    }
+    
+    end {
+    }
+}
 function New-ConfigurationDocument {
     [CmdletBinding()]
     param (
@@ -131,6 +234,140 @@ function New-ConfigurationDocument {
     }
 }
 
+
+enum MessageType{
+
+    Information = 0
+    Confirmation = 1
+    warning = 2
+    error = 3
+}
+
+Class LogDocument{
+    
+    
+    LogDocument(){
+        $ClassType = $this.GetType()
+        if($ClassType -eq [LogDocument]){
+            throw "this class cannot be instanciated, it must be inherited."
+        }
+    }
+    writelog(){
+        throw("Must be overwritten!")
+    }
+}
+
+Class LogFile : LogDocument {
+
+    [System.IO.FileInfo]$File
+    [String]$FileName
+    [string]$Folder
+
+
+    LogFile (){
+        
+        $CurrentInvocation = $MyInvocation.MyCommand.Definition 
+        $PSCommandPath = $PSCommandPath
+        $location = (Get-Location).Path
+       
+
+
+        $stack = Get-PSCallStack
+        If($stack.Location -eq "<No File>"){
+            if(($stack | measure).Count -ge 2){
+                $cp = (Get-PSCallStack)[-2].ScriptName
+            }else{
+
+                $cp = $PSCommandPath
+            }
+        }else{
+
+            $cp = (Get-PSCallStack)[-1].ScriptName #$PSCommandPath #Split-Path -parent $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath(ï¿½.\ï¿½) #$PSCommandPath
+        }
+
+        
+        $sr = $psScriptRoot
+        
+        $Extension = (get-item -Path $cp).Extension
+
+        $leaf = Split-Path $cp -Leaf
+
+        $Name = $leaf.Replace($Extension,"")
+        #$Name = $MyInvocation.ScriptName.Replace(".ps1","")
+        $timestamp = get-date -uformat '%Y%m%d-%T'
+        $timestamp = $timestamp.Replace(':','')
+        $FullFileName = $Name + '_' + $timestamp + '.log'
+        $ScriptRoot = split-Path -Path $cp -Parent
+        $LogFolderPath = Join-Path -Path $ScriptRoot -ChildPath 'logs'
+        #$LogFolderPath = join-Path -Path $psScriptRoot -ChildPath 'logs'
+        if (!(test-Path $LogFolderPath)){
+            mkdir $LogFolderPath
+        }
+        $FullPath = Join-Path -Path $LogFolderPath -ChildPath $FullFileName
+
+        $this.File = $FullPath
+        $this.FileName = $FullFileName
+        $this.Folder = $LogFolderPath
+
+       
+
+        #Add cleaning part
+     
+    }
+
+    LogFile ([string]$Path){
+    
+            $FullFileName = $this.CreateFileName()
+            
+              
+            
+            if (!(Test-Path $Path)){
+                $null = mkdir $Path
+            }
+            
+            $FullPath = Join-Path -Path $Path -ChildPath $FullFileName
+            
+            $this.File = $FullPath
+            $this.FileName = $FullFileName
+            $this.Folder = $Path
+
+            #$this.LogFileConfig = [LogFileConfig]::New($this)
+        
+    }
+
+    WriteLog([LogMessage]$Message){
+       
+            #To change to String builder?
+
+            $Writer = [System.IO.StreamWriter]::($This.Path,$true)
+            $Writer.write($Message.ToString())
+            #add-content -Path $this.File.FullName -Value $Message.ToString()
+    }
+
+    hidden [string] CreateFileName() {
+        $cp = $PSCommandPath #Split-Path -parent $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath(ï¿½.\ï¿½) #$PSCommandPath
+        #Write-Host "cp: $($cp)" -ForegroundColor DarkCyan
+        $sr = $psScriptRoot
+        $Extension = (get-item -Path $cp).Extension
+
+        $leaf = Split-Path $cp -Leaf
+
+        $Name = $leaf.Replace($Extension,"")
+        #$Name = $MyInvocation.ScriptName.Replace(".ps1","")
+        $timestamp = get-date -uformat '%Y%m%d-%T'
+        $timestamp = $timestamp.Replace(':','')
+        $FullFileName = $Name + '_' + $timestamp + '.log'
+
+
+        Return $FullFileName
+    }
+    
+    [String] ToString(){
+        Return "[{0}] Logs have written to: {1} " -f $this.Type.ToString(),$this.File
+    }
+
+}
+
 Class Logger{
     [System.IO.FileInfo]$Logfile
     
@@ -138,12 +375,84 @@ Class Logger{
         #$this.Logfile = $PSHTML_CONFIG.Logging.Path
         $this.Logfile = "$env:HOME/Logging.log"
     }
+    Logger ($Path){
+        $IOF = [System.IO.FileInfo]$Path
+        If(!($IOF.Directory.Exists)){
+            $IOF.Directory.Create()
+        }
+        
+        $this.Logfile = $Path
+    }
 
     Log($Message){
-        $sw = [System.IO.StreamWriter]::new($this.LogFile, [System.Text.Encoding]::UTF8)
+        $sw = [System.IO.StreamWriter]::new($this.LogFile,$true,[System.Text.Encoding]::UTF8)
         $sw.WriteLine($Message)
         $sw.Close()
     }
+}
+
+Class LogMessage {
+    [MessageType]$Type
+    $message
+    $Timestamp 
+    hidden $TypePrefix
+
+    LogMessage (){
+        $this.Timestamp = get-date -uformat '%Y%m%d-%T'
+    }
+    LogMessage ($Message){
+        $this.Message = $Message
+        $this.Timestamp = get-date -uformat '%Y%m%d-%T'
+    }
+    LogMessage([String]$Message,[MessageType]$type){
+        $this.Message = $Message
+        $this.Type = $type
+        $this.Timestamp = get-date -uformat '%Y%m%d-%T'
+    }
+
+    [String] ToString(){
+
+        $this.typeprefix = '[OK   ]'
+        switch ($this.type){
+            information {
+                $eventType = "Information"
+                $this.typeprefix = 'INFO '
+                break;
+            }
+            warning{
+                $eventType = "Warning"
+                $this.typeprefix = 'WARN '
+                break;
+            }
+            error{
+                $eventType = "Error"
+                $this.typeprefix = 'ERROR'
+                break;
+            }
+            default {
+                $eventType = "Information"
+                $this.typeprefix = 'INFO '
+               
+            }
+        }
+
+        return $this.FormatMessage()
+        #[$this.timestamp] + $typeprefix + $this.Message
+    }
+    [String]FormatMessage(){
+        return "[{0}][{1}] -> {2}" -f $this.Timestamp,$this.Typeprefix,$this.message
+    }
+
+    Static [String] FormatMessage([String]$MessageType,[String]$msg){
+        $Ts = get-date -uformat '%Y%m%d-%T'
+        If(!($MessageType)){
+            $MessageType = "INFO "
+
+        }
+
+        return "[{0}][{1}] -> {2}" -f $Ts,$MessageType,$msg
+    }
+
 }
 
 
@@ -1395,6 +1704,9 @@ html{
 
     Throw "This function has been renamed to 'Get-PSHTMLTemplate' and will be removed in a future release .Either use the Alias 'include' or rename your function calls from Get-PSHTMLTemplate to Get-PSHTMLTemplate"
 }
+Function Get-LogfilePath{
+    return $Script:Logfile
+}
 Function Get-ModuleRoot {
     [CmdletBinding()]
     Param(
@@ -1632,6 +1944,97 @@ Function Set-HtmlTag {
 
     }
 }
+function Write-Error {
+    <#
+        .SYNOPSIS
+            Proxy function for Write-Error that adds a timestamp and write the message to a log file.
+    #>
+        [CmdletBinding()]
+        param (
+            [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName)]
+            [AllowNull()]
+            [System.String] $Message
+        )
+        process {
+    
+            if (-not [System.String]::IsNullOrEmpty($Message)) {
+    
+                #$Message = Get-FormattedMessage -Message $Message;
+                $Type = 'Error'
+                $Msgobj = [LogMessage]::new($Message,$Type)
+                $FormatedMessage = $Msgobj.ToString()
+                Write-PSHTMLLog -message $FormatedMessage -type $Type
+                #Microsoft.PowerShell.Utility\Write-Error -Message $FormatedMessage;
+                #Overwriting Write-Error would not display message. See https://stackoverflow.com/questions/4998173/how-do-i-write-to-standard-error-in-powershell
+                $host.ui.WriteErrorLine($FormatedMessage)
+            }
+    
+        } # end process
+    } #end function
+Function Write-PSHTMLLog {
+    [Cmdletbinding()]
+    Param(
+        [String]$Message,
+
+        [ValidateSet("Info","Warning","Error","Information")]
+        [String]$Type = "info"
+    )
+
+    
+    $Script:Logger.Log($Message)
+
+
+}
+function Write-Verbose {
+    <#
+        .SYNOPSIS
+            Proxy function for Write-verbose that adds a timestamp and write the message to a log file.
+    #>
+        [CmdletBinding()]
+        param (
+            [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName)]
+            [AllowNull()]
+            [System.String] $Message
+        )
+        process {
+    
+            if (-not [System.String]::IsNullOrEmpty($Message)) {
+    
+                #$Message = Get-FormattedMessage -Message $Message;
+                $Type = 'Information'
+                $Msgobj = [LogMessage]::new($Message,$Type)
+                $FormatedMessage = $Msgobj.ToString()
+                Write-PSHTMLLog -message $FormatedMessage -type $Type
+                Microsoft.PowerShell.Utility\Write-Verbose -Message $FormatedMessage;
+            }
+    
+        } # end process
+    } #end function
+function Write-Warning {
+    <#
+        .SYNOPSIS
+            Proxy function for Write-Warning that adds a timestamp and write the message to a log file.
+    #>
+        [CmdletBinding()]
+        param (
+            [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName)]
+            [AllowNull()]
+            [System.String] $Message
+        )
+        process {
+    
+            if (-not [System.String]::IsNullOrEmpty($Message)) {
+    
+                #$Message = Get-FormattedMessage -Message $Message;
+                $Type = 'Warning'
+                $Msgobj = [LogMessage]::new($Message,$Type)
+                $FormatedMessage = $Msgobj.ToString()
+                Write-PSHTMLLog -message $FormatedMessage -type $Type
+                Microsoft.PowerShell.Utility\Write-Warning -Message $FormatedMessage;
+            }
+    
+        } # end process
+    } #end function
 Function a {
     <#
         .SYNOPSIS
@@ -6856,11 +7259,11 @@ function Write-PSHTMLSymbol {
 
 $ScriptPath = Split-Path -Path $MyInvocation.MyCommand.Path
 
-Write-Verbose "Loading aliases:"
 New-Alias -Name Include -Value 'Get-PSHTMLTemplate' -Description "Include parts of PSHTML documents using Templates" -Force
 
-
 $ConfigFile = Join-Path -Path $ScriptPath -ChildPath "pshtml.configuration.json"
-Write-Verbose "Loading data ConfigFile: $($ConfigFile)."
 
-$Script:PSHTML_CONFIGURATION = New-ConfigurationDocument -Path $ConfigFile -Force
+#Setting module variables
+    $Script:PSHTML_CONFIGURATION = New-ConfigurationDocument -Path $ConfigFile -Force
+    $Script:Logfile = $Script:PSHTML_CONFIGURATION.GetLogFilePath()
+    $Script:Logger = [Logger]::New($Script:LogFile)
