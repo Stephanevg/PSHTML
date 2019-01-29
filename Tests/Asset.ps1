83de40264c0ae665f8ef3b4714413389cc9e6a1b
stephanevg <svangulick@gmail.com>
2019-01-30 00:05:26 +0100

    Addind Asset pester test

---
+++ Tests/Asset.ps1
@@ -0,0 +1,20 @@
+$TestsPath = Split-Path $MyInvocation.MyCommand.Path
+
+#$FunctionsPath = join-Path -Path (get-item $TestsPath).Parent -ChildPath "Functions"
+
+$RootFolder = (get-item $TestsPath).Parent
+
+Push-Location -Path $RootFolder.FullName
+
+set-location -Path $RootFolder.FullName
+
+Write-Verbose "Importing module"
+
+import-module .\PSHTML -Force
+
+InModuleScope PSHTML {
+
+
+    $Config = Get-PSHTMLConfiguration
+
+}
