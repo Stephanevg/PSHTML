17753317323e35604772f1c1959e0189c13a0a65
stephanevg <svangulick@gmail.com>
2019-02-13 11:58:57 +0100

    added build with Out-PSHTMLDocument

--- PSHTML/PSHTML.psm1
+++ PSHTML/PSHTML.psm1
@@ -1,4 +1,4 @@
-﻿#Generated at 02/12/2019 22:49:06 by Stephane van Gulick
+﻿#Generated at 02/13/2019 11:58:03 by Stephane van Gulick
 
 Enum SettingType {
     General
@@ -2775,56 +2775,9 @@ Function ConvertTo-HTMLTable {
 
     <#
     .SYNOPSIS
-        Converts a powershell object to a HTML table.
+        This cmdlet is deprecated. Use ConvertTo-PSHTMLTable instead.
     
-    .DESCRIPTION
-        This cmdlet is intended to be used when powershell objects should be rendered in an HTML table format.
     
-    .PARAMETER Object
-        Specifies the object to use
-    
-    .PARAMETER Properties
-        Properties you want as table headernames
-    
-    .EXAMPLE
-        $service = Get-Service -Name Sens,wsearch,wscsvc | Select-Object -Property DisplayName,Status,StartType
-        ConvertTo-HTMLtable -Object $service
-    
-    .EXAMPLE
-        $proc = Get-Process | Select-Object -First 2
-        ConvertTo-HTMLtable -Object $proc 
-    
-    .EXAMPLE
-        $proc = Get-Process | Select-Object -First 2
-        ConvertTo-HTMLtable -Object $proc -properties name,handles
-    
-        Returns the following HTML code
-    
-        <table>
-            <thead>
-                <tr>
-                    <td>name</td>
-                    <td>handles</td>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>AccelerometerSt</td>
-                    <td>155</td>
-                </tr>
-                <tr>
-                    <td>AgentService</td>
-                    <td>190</td>
-                </tr>
-            </tbody>
-        </table>
-    
-    .NOTES
-            Current version 0.7.1
-            History:
-            2018.05.09;stephanevg;Made Linux compatible (changed Get-Serv).
-            2018.10.14;Christophe Kumor;Update.
-            2018.05.09;stephanevg;Creation.
             
     
     .LINK
@@ -2836,59 +2789,8 @@ Function ConvertTo-HTMLTable {
         $Object,
         [String[]]$Properties
     )
-    
-    
-    if ($Properties) {
-        $HeaderNames = $Properties
-    }
-    else {
-        $props = $Object | Get-Member -MemberType Properties | Select-Object Name
-        $HeaderNames = @()
-        foreach ($i in $props) {
-            $HeaderNames += $i.name.tostring()
-
-        }
-
-    }
-    table {
-    
-        thead {
-    
-            tr {
-    
-                foreach ($Name in $HeaderNames) {
-    
-                    td {
-                        $Name
-                    }
-    
-                }
-    
-            }
-    
-        }
-    
-        tbody {
-    
-            foreach ($item in $Object) {
-    
-                tr {
-    
-                    foreach ($propertyName in $HeaderNames) {
-    
-                        td {
-                            $item.$propertyName
-                        }
-    
-                    }
-    
-                }
-
-            }
-    
-        }
-
-    }
+    Write-Warning "ConvertTo-HTMLTable is deprecated and will be removed in a future version. Please use ConvertTo-PSHTMLTable instead"
+    ConvertTo-PSHTMLTable @PSBoundParameters
 }
 
 Function ConvertTo-PSHTMLTable {
@@ -6571,6 +6473,61 @@ Function option {
 
 }
 
+function OUT-PSHTMLDocument {
+    <#
+    .SYNOPSIS
+    Outputs the HTML document to an output location
+    .DESCRIPTION
+        Output the html string into a file.
+    .EXAMPLE
+        The following example gets the list of first 5 processes. Converts it into an HTML Table. It outputs the results in a file, and opens the results imÃ©diatley.
+
+        $o = Get-PRocess | select ProcessName,Handles | select -first 5
+        $FilePath = "C:\temp\OutputFile.html"
+        $E = ConvertTo-HTMLTable -Object $o 
+        $e | Out-PSHTMLDocument -OutPath $FilePath -Show
+
+    .INPUTS
+        String
+    .OUTPUTS
+        None
+    .NOTES
+
+        Author: StÃ©phane van Gulick
+                
+        
+    .LINK
+        https://github.com/Stephanevg/PSHTML
+#>
+    [CmdletBinding()]
+    param (
+        $OutPath = {Throw "Must provide a path"},
+
+        [Parameter(ValueFromPipeline = $true)]
+        $HTMLDocument = {Throw "HTMLDocument cannot be empty"},
+
+        [Parameter(Mandatory = $False)]
+        [Switch]$Show
+    )
+    
+    begin {
+        $Writer = [System.IO.StreamWriter]$OutPath
+    }
+    
+    process {
+        #[System.IO.TextWriter]
+        Foreach ($Line in $HTMLDocument) {
+            $writer.WriteLine($Line, "utf8")
+        }
+    }
+    
+    end {
+        $Writer.Close()
+        If ($Show) {
+            Invoke-Item -Path $OutPath
+        }
+    }
+}
 Function output {
     <#
     .SYNOPSIS
