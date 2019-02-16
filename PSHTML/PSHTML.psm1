f20f5a02e7f532e68e7df3101b7abd132c9fff5d
stephanevg <svangulick@gmail.com>
2019-02-16 11:44:53 +0100

    fixes for #212 and #211 (Added enctype to form. removed mandatory target)

--- PSHTML/PSHTML.psm1
+++ PSHTML/PSHTML.psm1
@@ -1,4 +1,4 @@
-﻿#Generated at 02/13/2019 14:18:03 by Stephane van Gulick
+#Generated at 02/16/2019 11:30:12 by Stephane van Gulick
 
 Enum SettingType {
     General
@@ -3607,6 +3607,37 @@ Function Form {
     .SYNOPSIS
     Generates Form HTML tag.
 
+    .DESCRIPTION 
+    Generates the HTML FORM tag for form manipulations.
+
+    .PARAMETER Action
+    Specifiy which URL should be called when the form is called.
+
+    .PARAMETER Method
+    The HTTP method to use when sending form-data.
+    Acceptable values are:
+    GET
+    POST
+
+    .PARAMETER Target
+    Specifies where to display the response that is received after submitting the form.
+    Acceptable values are:
+    _blank
+    _self (default)
+    _parent
+    _top
+
+    If this parameter is not called, it will default to target=_self
+
+    .Parameter Enctyp
+    This is a dynamic parameter, which is only available when 'Method' is set to 'post'.
+    The available values are:
+    -"application/x-www-form-urlencoded"
+    -"multipart/form-data"
+    -"text/plain"
+
+    See here for more information --> https://www.w3schools.com/tags/att_form_enctype.asp
+
     .PARAMETER Class
     Allows to specify one (or more) class(es) to assign the html element.
     More then one class can be assigned by seperating them with a white space.
@@ -3653,7 +3684,7 @@ Function Form {
         [ValidateSet("get", "post")]
         [String]$method = "get",
 
-        [Parameter(Mandatory = $true, Position = 2)]
+        [Parameter(Mandatory = $false, Position = 2)]
         [ValidateSet("_blank", "_self", "_parent", "top")]
         [String]$target,
 
@@ -3676,7 +3707,24 @@ Function Form {
         )]
         $Content
     )
+    DynamicParam{
+        If($method -eq 'Post'){
+            $ParameterName = 'enctype'
+            $Attribute = New-Object System.Management.Automation.ParameterAttribute
+            $Attribute.HelpMessage = "Please enter encoding type"
+            $Attribute.Mandatory = $True
 
+            $ValidateSetValues = @("application/x-www-form-urlencoded","multipart/form-data","text/plain")
+            $ValidateSet = New-Object System.Management.Automation.ValidateSetAttribute($ValidateSetValues)
+            $attributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+            $attributeCollection.add($ValidateSet)
+            $attributeCollection.Add($Attribute)
+            $Para = New-Object System.Management.Automation.RuntimeDefinedParameter($ParameterName, [string], $attributeCollection)
+            $paramDictionary = new-object System.Management.Automation.RuntimeDefinedParameterDictionary
+            $paramDictionary.Add($ParameterName, $para)
+            return $paramDictionary
+        }
+    }
     Process {
         $tagname = "form"
 
