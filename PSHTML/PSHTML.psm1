b1963c7aa307a82bb5382f4bc8464f0db560a8df
joshburkard <josh@burkard.it>
2019-06-19 20:53:08 +0200

    function i added

--- PSHTML/PSHTML.psm1
+++ PSHTML/PSHTML.psm1
@@ -1,4 +1,4 @@
-﻿#Generated at 06/19/2019 20:51:30 by Stephane van Gulick
+﻿#Generated at 06/19/2019 20:52:34 by Stephane van Gulick
 
 Enum SettingType {
     General
@@ -4914,6 +4914,69 @@ Function html {
         Set-HtmlTag -TagName $tagname -Parameters $PSBoundParameters -TagType nonVoid
     }
 }
+Function i {
+    <#
+        .SYNOPSIS
+
+        Generates a <i> HTML tag.
+        The <a> tag defines a hyperlink, which is used to link from one page to another.
+        
+        .DESCRIPTION
+
+        .PARAMETER Class
+        Allows to specify one (or more) class(es) to assign the html element.
+        More then one class can be assigned by seperating them with a white space.
+
+        .PARAMETER Id
+        Allows to specify an id to assign the html element.
+
+        .PARAMETER Content
+        Allows to add child element(s) inside the current opening and closing HTML tag(s).
+
+
+        .EXAMPLE
+        The following exapmles show cases how to create an empty a, with a class, an ID, and, custom attributes.
+        
+        i -Class "myclass1 MyClass2" -Id myid -Attributes @{"custom1"='val1';custom2='val2'}
+
+        Generates the following code:
+
+        <i Class="myclass1 MyClass2" Id="myid" custom1="val1" custom2="val2"  >
+        </i>
+
+
+        .NOTES
+        Current version 3.1
+        History:
+            2019.06.19;@Josh_Burkard;initial version
+        .LINK
+            https://github.com/Stephanevg/PSHTML
+    #>
+
+    Param(
+
+        [Parameter(Mandatory = $false)]
+        [AllowEmptyString()]
+        [AllowNull()]
+        $Content,
+
+        [AllowEmptyString()]
+        [AllowNull()]
+        [String]$Class,
+
+        [String]$Id,
+        
+        [Hashtable]$Attributes
+
+    )
+    $tagname = "i"
+
+    Set-htmltag -TagName $tagName -Parameters $PSBoundParameters -TagType NonVoid
+    
+    
+
+}
+ 
 Function img {
     <#
         .SYNOPSIS
