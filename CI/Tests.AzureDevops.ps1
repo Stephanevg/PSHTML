9d06d2370bc5df01691f8a0870c18a5ac2429d07
Stéphane van Gulick <svangulick@gmail.com>
2019-06-27 23:31:35 +0200

    update

--- CI/Tests.AzureDevops.ps1
+++ CI/Tests.AzureDevops.ps1
@@ -1,3 +1,4 @@
 
 $TestsFolder = "$($env:Build.SourcesDirectory)/Tests"
+set-location $TestsFolder
 $res = Invoke-Pester -Path $TestsFolder -OutputFormat NUnitXml -OutputFile TestsResults.xml -PassThru #-CodeCoverage $TestFiles
