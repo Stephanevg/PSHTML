c6383914649d9854f566da750b3c9cf38af87517
Stéphane van Gulick <svangulick@gmail.com>
2019-06-27 23:58:17 +0200

    fixed variable names

--- CI/Tests.AzureDevops.ps1
+++ CI/Tests.AzureDevops.ps1
@@ -1,7 +1,7 @@
 
 install-module pester -Force
-$TestsFolder = "$($env:Build.SourcesDirectory)/Tests"
+$TestsFolder = "$($env:Build_SourcesDirectory)/Tests"
 gci env:
-write-host "sourcedirectory = $($env:Build.SourcesDirectory)"
+write-host "sourcedirectory = $($env:Build_SourcesDirectory)"
 set-location $TestsFolder
 $res = Invoke-Pester -Path $TestsFolder -OutputFormat NUnitXml -OutputFile TestsResults.xml -PassThru #-CodeCoverage $TestFiles
