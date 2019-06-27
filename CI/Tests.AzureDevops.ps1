703072a5769d779104e00c3e86ea529b97247729
Stéphane van Gulick <svangulick@gmail.com>
2019-06-27 23:47:34 +0200

    added output vars

--- CI/Tests.AzureDevops.ps1
+++ CI/Tests.AzureDevops.ps1
@@ -1,5 +1,7 @@
 
-install-module pester
+install-module pester -Force
 $TestsFolder = "$($env:Build.SourcesDirectory)/Tests"
+gci $env:
+write-host "sourcedirectory = $($env:Build.SourcesDirectory)"
 set-location $TestsFolder
 $res = Invoke-Pester -Path $TestsFolder -OutputFormat NUnitXml -OutputFile TestsResults.xml -PassThru #-CodeCoverage $TestFiles
