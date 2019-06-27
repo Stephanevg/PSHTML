6030c5cabb930180c259c87b40e727bf65c37d76
Stéphane van Gulick <svangulick@gmail.com>
2019-06-28 00:23:54 +0200

    added throw on fail

--- CI/Tests.AzureDevops.ps1
+++ CI/Tests.AzureDevops.ps1
@@ -5,3 +5,7 @@ write-host "sourcedirectory = $($env:BUILD_SOURCESDIRECTORY)"
 gci $env:BUILD_SOURCESDIRECTORY
 set-location $TestsFolder
 $res = Invoke-Pester -Path $TestsFolder -OutputFormat NUnitXml -OutputFile TestsResults.xml -PassThru #-CodeCoverage $TestFiles
+
+if ($res.FailedCount -gt 0 -or $res.PassedCount -eq 0) { 
+    throw "$($res.FailedCount) tests failed - $($res.PassedCount) successfully passed"
+};
