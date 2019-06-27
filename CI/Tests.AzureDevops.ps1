80d43e1aace6dd841dcc8544f06cfbd44a290e22
Stéphane van Gulick <svangulick@gmail.com>
2019-06-28 00:06:30 +0200

    typo

--- CI/Tests.AzureDevops.ps1
+++ CI/Tests.AzureDevops.ps1
@@ -2,6 +2,6 @@
 install-module pester -Force
 $TestsFolder = "$($env:Build_SourcesDirectory)/Tests"
 write-host "sourcedirectory = $($env:Build_SourcesDirectory)"
-gci $env:Build_SourcesDirectory)
+gci $env:Build_SourcesDirectory
 set-location $TestsFolder
 $res = Invoke-Pester -Path $TestsFolder -OutputFormat NUnitXml -OutputFile TestsResults.xml -PassThru #-CodeCoverage $TestFiles
