73f1fe657ddf04021a0b050b15cc4272e3646cb9
Stéphane van Gulick <svangulick@gmail.com>
2019-06-27 23:32:21 +0200

    added pester install

--- CI/Tests.AzureDevops.ps1
+++ CI/Tests.AzureDevops.ps1
@@ -1,4 +1,5 @@
 
+install-module pester
 $TestsFolder = "$($env:Build.SourcesDirectory)/Tests"
 set-location $TestsFolder
 $res = Invoke-Pester -Path $TestsFolder -OutputFormat NUnitXml -OutputFile TestsResults.xml -PassThru #-CodeCoverage $TestFiles
